fbf508fd63fa009a151173f915f443095b0f3e20
Oto Lopes <otoengc@gmail.com>
2019-09-27 17:41:56 -0300

    Iniciando jornada

--- warriorjs/xogun-baby-steps level 2/Player.js
+++
@@ -1,9 +0,0 @@
-class Player {
-  playTurn(warrior) {
-    // xogun analyzes
-    if (! warrior.feel().isEmpty()) // xogun attack
-      warrior.attack()
-    else // xogun walks
-      warrior.walk()
-  }
-}
